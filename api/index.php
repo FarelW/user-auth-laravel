5828aaf48bcb5d3c1cc3d037f79fdee079b69ab8
FarelW <eeksapi321@gmail.com>
2024-03-03 22:57:17 +0700

    fix : index.php

--- api/index.php
+++ api/index.php
@@ -1,16 +1,4 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
